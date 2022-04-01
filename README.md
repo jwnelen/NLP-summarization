ed682c440fe0c83041f7075d49688b83f022fe8f
Jeroen Nelen <jwnelen@gmail.com>
2022-04-01 15:29:51 +0200

    added requirements and other small changes

---
+++ README.md
@@ -0,0 +1,3 @@
+Conda env that can be used as kernel in Jupyter:
+```ipython kernel install --user --name NLP-summarization```
+
--- main_py.py
+++ main_py.py
@@ -6,7 +6,4 @@ raw_dataset = load_dataset('cnn_dailymail', '3.0.0')
 classifier = pipeline("summarization")
 
 if __name__ == '__main__':
-    for i in range(10):
-        article = raw_dataset["train"][i]
-        summary = classifier(article)
-        print(summary)
+    pass
